cb19a75c50f581f08d0a30266589a7f06cfd214d
hyeryn2000 <hyeryn2000@naver.com>
2020-09-29 01:42:19 +0900

    실수저장 스택 수정본

--- StackStore.java
+++ StackStore.java
@@ -9,36 +9,60 @@ interface Stack{
 }
 
 class StringStack implements Stack{
-	public int length() { 
-		return 0;
+	private int num; // 저장 가능한 개수
+	private int set; // 저장 인덱스
+	private String[] stack;
+	public StringStack(int n) { // 입력한 개수만큼 main에서 받아와야 함
+		this.num = n;
+		this.set = 0;
+		stack = new String[num]; // 받은 개수의 크기 만한 배열을 선언
 	}
-	public int capacity() { // 입력받은 크기가 main에 있는데 어떻게 하지? 불러와야 될 것 같은데 말이 됨?
-		return 0;
+	public int length() { 
+		return set;
+	}
+	public int capacity() { 
+		return stack.length;
 	}
 	public String pop() { // 스택의 특성에 맞게 하나씩 pop하기
-		return null;
+		if(set<=0)
+			return null; // 스택이 비어있을때
+		set--;
+		String s = stack[set];
+		return s;
 	}
 	public boolean push(String val) { // 가능한지 판별하고 성공하면 true를 리턴하자
-		return false;
+		if(set<num) {
+			stack[set] = val;
+			set++;
+			return true;
+		}
+		else
+			return false;
 	}
 }
 
 public class StackStore {
 	static void run(){
-		StringStack ss = new StringStack();
 		Scanner sc = new Scanner(System.in);
 		int size = sc.nextInt();
+		StringStack ss = new StringStack(size);
 		while(true) {
+			int num = 0;
 			System.out.print("문자열 입력 >> ");
 			String word = sc.next();
 			if(word.equals("그만"))
-				break;
-			else {
-				
+				break; // 저장 그만하고 그대로 while문을 빠져나옴
+			if(!ss.push(word)) {
+				System.out.println("스택이 꽉 차서 푸시 불가!");
 			}
 		}
-		System.out.println("스택에 저장된 모든 문자열 팝 : ");
-		ss.pop();
+		System.out.print("스택에 저장된 모든 문자열 팝 : ");
+		int len = ss.length();
+		for(int i = 0; i<len;i++) {
+			String s = ss.pop();
+			System.out.print(s+" ");
+		}
+		sc.close();
 	}
 	
 	public static void main(String[] args) {
